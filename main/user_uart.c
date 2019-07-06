6d7c9073dfbfb687893e54f7c3eaa2f1b7ae537c
PURESUN-OYLY <Mrouyang@live.com>
2019-07-06 14:31:29 +0800

    SSH Key test

--- main/user_uart.c
+++ main/user_uart.c
@@ -168,4 +168,5 @@ void Wifi_Start_AP()
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+    
 }
